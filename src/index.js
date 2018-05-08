7448eda307321258caf027a8131c30c6d0301081
TianXing Li <tianxingli24@gmail.com>
2018-05-07 20:03:17 -0400

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// const store = createStore(reducers);
+// const unsubsribe = store.subscribe(() => {
+//     console.log(store.getState());
+// });
+
+ReactDOM.render(
+    <div> asldkfjasl ;kj </div>,
+document.getElementById("app"));
