5a76fb81c428933512fd5ee79700438774ebc05c
TianXing Li <tianxingli24@gmail.com>
2018-05-07 21:13:11 -0400

    Added crappy splitter demo.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
+import React from 'react'
 
-// const store = createStore(reducers);
-// const unsubsribe = store.subscribe(() => {
-//     console.log(store.getState());
-// });
+import {
+  ReflexContainer,
+  ReflexSplitter,
+  ReflexElement
+} from 'react-reflex'
+
+import './style.scss';
+import 'react-reflex/styles.css';
+
+/////////////////////////////////////////////////////////
+// Re-Flex basic vertical layout with resizable splitter
+//
+/////////////////////////////////////////////////////////
+class ReflexBasicSplitterDemo
+  extends React.Component {
+
+  render () {
+
+    return (
+      <ReflexContainer orientation="vertical">
+
+        <ReflexElement className="left-pane">
+          <div className="pane-content">
+            <label>
+              Left Pane (resizable)
+            </label>
+          </div>
+        </ReflexElement>
+
+        <ReflexSplitter/>
+
+        <ReflexElement className="right-pane"
+          minSize="200"
+          maxSize="800">
+          <div className="pane-content">
+            <label>
+              Right Pane (resizable)
+              <br/>
+              <br/>
+              minSize = 200px
+              <br/>
+              maxSize = 800px
+            </label>
+          </div>
+        </ReflexElement>
+
+      </ReflexContainer>
+    )
+  }
+}
 
 ReactDOM.render(
-    <div> asldkfjasl ;kj </div>,
-document.getElementById("app"));
+    <ReflexBasicSplitterDemo/>
+  ,
+  document.getElementById('demo-basic-splitter'))
