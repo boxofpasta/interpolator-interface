2c2e66dc61ef584ebc716ff3b0704559878333cf
TianXing Li <tianxingli24@gmail.com>
2018-05-10 15:09:22 -0400

    Moved core implementation code from index.js to App.js.

---
+++ src/components/App.js
@@ -0,0 +1,69 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import {
+  ReflexContainer,
+  ReflexSplitter,
+  ReflexElement
+} from 'react-reflex';
+
+// Custom component imports.
+import MainTabView from './MainTabView';
+import MenuBar from './MenuBar';
+
+// Theme imports.
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {deepOrange500} from 'material-ui/styles/colors';
+
+// CSS imports.
+import './style.css';
+import './reflexstyle.css';
+
+// Global theme.
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  }
+});
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <MenuBar/>
+        <div id="layout">
+          <ReflexContainer orientation="horizontal">
+            <ReflexElement>
+              <ReflexContainer orientation="vertical">
+                <ReflexElement>
+                  <MainTabView/>
+                </ReflexElement>
+
+                <ReflexSplitter/>
+
+                <ReflexElement size="700">
+                  <div>
+                    <label>
+                      Video preview.
+                    </label>
+                  </div>
+                </ReflexElement>
+              </ReflexContainer>
+            </ReflexElement>
+
+            <ReflexSplitter/>
+
+            <ReflexElement size="300">
+              <div>
+                <label>
+                  Timeline.
+                </label>
+              </div>
+            </ReflexElement>
+          </ReflexContainer>
+        </div>
+      </MuiThemeProvider>
+    )
+  }
+}
