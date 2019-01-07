1f812d36f06f6f5cb8964c970dd9d18c0185ddba
Subtlemon <harryliang1@gmail.com>
2019-01-07 14:55:19 -0500

    Overlayed two images

--- src/mainhintwindow.js
+++ src/mainhintwindow.js
@@ -13,17 +13,34 @@ export default class MainHintWindow extends React.Component {
 
     // Set initial state.
     this.state = {
-      flowMode: 'drag'
+      flowMode: 'drag',
+      image_opacity: '0.4'
     };
   }
 
   render() {
+    const img_0_url = '../0000001_0.png';
+    const img_1_url = '../0000001_1.png';
+    const img_0_style = {
+        position: 'absolute',
+        top: '0px',
+        left: '0px'
+    };
+    var img_1_style = {
+        position: 'absolute',
+        top: '0px',
+        left: '0px', 
+        opacity: this.state.image_opacity
+    };
     return (
       <ReflexContainer orientation="horizontal">
         <ReflexElement>
           <ReflexContainer orientation="vertical">
             <ReflexElement>
-              <div><label>Image Viewer</label></div>
+              <div>
+                <img src={img_0_url} style={img_0_style} />
+                <img src={img_1_url} style={img_1_style} />
+              </div>
             </ReflexElement>
 
             <ReflexSplitter/>
